b33b6d73508b52fcdfc5725525415103bf3ac54a
Amon Ra II <palebluedot85@gmil.com>
2022-07-31 09:23:10 -0400

    first commit of the website

---
+++ script.js
@@ -0,0 +1,53 @@
+const prevBtn = document.querySelector(".carousel-prev-btn");
+const nextBtn = document.querySelector(".carousel-next-btn");
+const carousel = document.querySelector(".carousel");
+const carouselBox = document.querySelector(".carousel-box");
+const slides = document.querySelectorAll(".carousel-slide");
+
+const toggleBtn = document.querySelector(".toggle-btn");
+const navRibbon = document.querySelector(".nav-ribbon");
+
+let direction;
+
+nextBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  direction = 1;
+  carouselBox.style.justifyContent = "flex-start";
+  slides.forEach((slide) => {
+    carousel.style.transform = `translate(-34rem)`;
+  });
+});
+
+prevBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (direction === 1) {
+    carousel.appendChild(carousel.firstElementChild);
+    direction = -1;
+  }
+  carouselBox.style.justifyContent = "flex-end";
+  slides.forEach((slide) => {
+    carousel.style.transform = `translate(34rem)`;
+  });
+});
+
+carousel.addEventListener("transitionend", () => {
+  direction === 1
+    ? carousel.appendChild(carousel.firstElementChild)
+    : carousel.prepend(carousel.lastElementChild);
+  carousel.style.transition = "none";
+  carousel.style.transform = `translate(0)`;
+  setTimeout(() => {
+    carousel.style.transition = "all 800ms ease";
+  });
+});
+
+toggleBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if(navRibbon.children[2].classList[0] === 'nav-btns') {
+    navRibbon.children[2].classList.remove('nav-btns', 'default-padding')
+    navRibbon.children[2].classList.add('nav-btn-list')
+  } else if(navRibbon.children[2].classList[0] === 'nav-btn-list') {
+    navRibbon.children[2].classList.remove('nav-btn-list')
+    navRibbon.children[2].classList.add('nav-btns', 'default-padding')
+  } 
+});
